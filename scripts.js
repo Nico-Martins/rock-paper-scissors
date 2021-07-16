98cf72ea12ea51e792e72c23ba91c33d21637ff5
nico <nicolasdeconcilis@gmail.com>
2021-07-16 16:00:00 -0300

    finished

--- scripts.js
+++ scripts.js
@@ -11,54 +11,96 @@ function computerPlay() {
     return option;
 }
 
-function cleanInput(word) {
-    let lowercaseWord = word.toLowerCase();
-    let firstLetter = lowercaseWord.charAt(0);
-    let uppercaseFirstLetter = firstLetter.toUpperCase();
-
-    console.log(lowercaseWord.replace(firstLetter, uppercaseFirstLetter));
-    return lowercaseWord.replace(firstLetter, uppercaseFirstLetter);
-}
-
 function play(playerSelection, computerSelection) {
     let outcome;
     
     switch (true) {
-        case (playerSelection === "Rock" && computerSelection === "Scissors"):
+        case (playerSelection === "Scissors" && computerSelection === "Paper"): 
+            outcome = [`You Win! ${playerSelection} cuts ${computerSelection}`, `won`]; /*Scissors cuts paper*/
+            break;
         case (playerSelection === "Paper" && computerSelection === "Rock"):
-        case (playerSelection === "Scissors" && computerSelection === "Paper"):
-            outcome = `You Win! ${playerSelection} beats ${computerSelection}`;
+            outcome = [`You Win! ${playerSelection} covers ${computerSelection}`, `won`]; /*Paper covers rock*/
+            break
+        case (playerSelection === "Rock" && computerSelection === "Lizard"):
+            outcome = [`You Win! ${playerSelection} crushes ${computerSelection}`, `won`]; /*Rock crushes lizard*/
             break;
-        case (playerSelection === "Scissors" && computerSelection === "Rock"):
-        case (playerSelection === "Rock" && computerSelection === "Paper"):
-        case (playerSelection === "Paper" && computerSelection === "Scissors"):
-            outcome = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        case (playerSelection === "Lizard" && computerSelection === "Spock"):
+            outcome = [`You Win! ${playerSelection} poisons ${computerSelection}`, `won`]; /*Lizard poisons Spock*/
             break;
+        case (playerSelection === "Spock" && computerSelection === "Scissors"):
+            outcome = [`You Win! ${playerSelection} smashes ${computerSelection}`, `won`]; /*Spock smashes scissors*/
+            break;
+        case (playerSelection === "Scissors" && computerSelection === "Lizard"):
+            outcome = [`You Win! ${playerSelection} decapitates ${computerSelection}`, `won`]; /*Scissors decapitates lizard*/
+            break;
+        case (playerSelection === "Lizard" && computerSelection === "Paper"):
+            outcome = [`You Win! ${playerSelection} eats ${computerSelection}`, `won`]; /*Lizard eats paper*/
+            break;
+        case (playerSelection === "Paper" && computerSelection === "Spock"):
+            outcome = [`You Win! ${playerSelection} disproves ${computerSelection}`, `won`]; /*Paper disproves Spock*/
+            break;
+        case (playerSelection === "Spock" && computerSelection === "Rock"):
+            outcome = [`You Win! ${playerSelection} vaporizes ${computerSelection}`, `won`]; /*Spock vaporizes rock*/
+            break;
+        case (playerSelection === "Rock" && computerSelection === "Scissors"):
+            outcome = [`You Win! ${playerSelection} crushes ${computerSelection}`, `won`]; /*Rock crushes scissors*/
+            break;
+
+
+        case (computerSelection === "Scissors" && playerSelection === "Paper"):
+            outcome = [`You Lose! ${computerSelection} cuts ${playerSelection}`, `lost`]; /*Scissors cuts paper*/
+            break;
+        case (computerSelection === "Paper" && playerSelection === "Rock"):
+            outcome = [`You Lose! ${computerSelection} covers ${playerSelection}`, `lost`]; /*Paper covers rock*/
+            break;
+        case (computerSelection === "Rock" && playerSelection === "Lizard"):
+            outcome = [`You Lose! ${computerSelection} crushes ${playerSelection}`, `lost`]; /*Rock crushes lizard*/
+            break;
+        case (computerSelection === "Lizard" && playerSelection === "Spock"):
+            outcome = [`You Lose! ${computerSelection} poisons ${playerSelection}`, `lost`]; /*Lizard poisons Spock*/
+            break;
+        case (computerSelection === "Spock" && playerSelection === "Scissors"):
+            outcome = [`You Lose! ${computerSelection} smashes ${playerSelection}`, `lost`]; /*Spock smashes scissors*/
+            break;
+        case (computerSelection === "Scissors" && playerSelection === "Lizard"):
+            outcome = [`You Lose! ${computerSelection} decapitates ${playerSelection}`, `lost`]; /*Scissors decapitates lizard*/
+            break;
+        case (computerSelection === "Lizard" && playerSelection === "Paper"):
+            outcome = [`You Lose! ${computerSelection} eats ${playerSelection}`, `lost`]; /*Lizard eats paper*/
+            break;
+        case (computerSelection === "Paper" && playerSelection === "Spock"):
+            outcome = [`You Lose! ${computerSelection} disproves ${playerSelection}`, `lost`]; /*Paper disproves Spock*/
+            break;
+        case (computerSelection === "Spock" && playerSelection === "Rock"):
+            outcome = [`You Lose! ${computerSelection} vaporizes ${playerSelection}`, `lost`]; /*Spock vaporizes rock*/
+            break;
+        case (computerSelection === "Rock" && playerSelection === "Scissors"):
+            outcome = [`You Lose! ${computerSelection} crushes ${playerSelection}`, `lost`]; /*Rock crushes scissors*/
+            break;
+            
+
         case (playerSelection === computerSelection):
-            outcome = `You Tied! ${playerSelection} ties with ${computerSelection}`;
+            outcome = [`You Tied! ${playerSelection} ties with ${computerSelection}`, `tied`];
             break;
-        default:
-            console.log("Oh no, something went horribly wrong!");
     }
 
     return outcome;
 }
 
-function game(){
-    for (let i = 1 ; i <= 5 ; i++){ 
-        console.log(`Round ${i}:`);
 
-        let playerSelection = cleanInput(prompt("Enter your choice:"));
-        while (playerSelection !== "Rock" && playerSelection !== "Paper" && playerSelection !== "Scissors") { 
-            playerSelection = cleanInput(prompt("Incorrect choice. Enter a valid choice:"));
-        } 
-        let computerSelection = computerPlay();
-        
+const buttons = Array.from(document.getElementsByClassName("human-button"));
+const resultParagraph = document.getElementById("result");
+const computerScore = document.getElementById("computer-score");
+const yourScore = document.getElementById("your-score");
 
-        console.log(`You chose: ${playerSelection}.\nThe computer chose: ${computerSelection}.`);
-
-        let result = play(playerSelection, computerSelection);
-        
-        console.log(result);
-    }
-}
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+        const result = play(button.textContent, computerPlay())
+        resultParagraph.textContent = result[0];
+        if (result[1] === `won`){
+            yourScore.textContent =  +yourScore.textContent + 1;
+        } else if (result[1] === `lost`){
+            computerScore.textContent =  +computerScore.textContent + 1;
+        }
+    });
+});
